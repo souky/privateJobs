dbd25c077bb96e9a3ea642fd56b7e9b92157f259
souky <136069803@qq.com>
2018-08-03 22:10:56 +0800

    page two

--- js/index.js
+++ js/index.js
@@ -1,14 +1,33 @@
 
 $(function(){
-  init_page_one();
+  init_page("#page_two");
 })
 
 function init_page_one(){
   var o = $("#page_one");
   o.find(".animate").each(function(){
     var names = $(this).attr("data-animate");
-    var sec = $(this).attr("data-sec");
-    $(this).css("animation",names+" "+sec+"s");
-    $(this).css("-webkit-animation",names+" "+sec+"s");
+    var sec_de = $(this).attr("data-secDe");
+    var sec_ = "";
+    if(sec_de){
+    	sec_ = sec_de+"s";
+    }
+    $(this).css("animation",names+" 1s"+" ease "+sec_+" both");
+    $(this).css("-webkit-animation",names+" 1s"+" ease "+sec_+" both");
+  })
+}
+
+
+function init_page(names){
+  var o = $(names);
+  o.find(".animate").each(function(){
+    var names = $(this).attr("data-animate");
+    var sec_de = $(this).attr("data-secDe");
+    var sec_ = "";
+    if(sec_de){
+    	sec_ = sec_de+"s";
+    }
+    $(this).css("animation",names+" 1s"+" ease "+sec_+" both");
+    $(this).css("-webkit-animation",names+" 1s"+" ease "+sec_+" both");
   })
 }
