82057c28195f949cf6c7d85d7f88702b1cc053c4
souky <136069803@qq.com>
2018-08-05 01:33:28 +0800

    js eff

--- js/index.js
+++ js/index.js
@@ -1,33 +1,74 @@
 
 $(function(){
-  init_page("#page_two");
+	init();
+  init_page("#page_one");
 })
 
-function init_page_one(){
-  var o = $("#page_one");
-  o.find(".animate").each(function(){
+function init_page(names){
+	if(names == undefined){
+		return;
+	}
+  var o = $(names);
+	o.find(".animate").each(function(){
     var names = $(this).attr("data-animate");
     var sec_de = $(this).attr("data-secDe");
+ 		var infi_de = $(this).attr("data-infinty");
     var sec_ = "";
+    var infinty = "";
     if(sec_de){
     	sec_ = sec_de+"s";
     }
-    $(this).css("animation",names+" 1s"+" ease "+sec_+" both");
-    $(this).css("-webkit-animation",names+" 1s"+" ease "+sec_+" both");
+    if(infi_de == '1'){
+    	infinty = "alternate infinite";
+    }
+    $(this).css("animation",names+" 1s"+" ease "+sec_+" both " + infinty);
+    $(this).css("-webkit-animation",names+" 1s"+" ease "+sec_+" both" + infinty);
   })
+  
 }
 
-
-function init_page(names){
-  var o = $(names);
-  o.find(".animate").each(function(){
-    var names = $(this).attr("data-animate");
-    var sec_de = $(this).attr("data-secDe");
-    var sec_ = "";
-    if(sec_de){
-    	sec_ = sec_de+"s";
-    }
-    $(this).css("animation",names+" 1s"+" ease "+sec_+" both");
-    $(this).css("-webkit-animation",names+" 1s"+" ease "+sec_+" both");
-  })
+function init(){
+	
+	$(".main").each(function(){
+		
+		$(this).swipeUp(function(){
+			var num = parseInt($(this).attr("data-order"));
+			num++;
+			if(num > 4){
+				return;
+			}
+			if(num == 4){
+				$(".bottom_common").hide();
+			}
+			var o = $(".main[data-order='"+num+"']");
+			if(o != undefined){
+				var id = o.attr("id");
+				var this_ = $(this);
+				this_.css("animation","swipeUp 1s ease both");
+				setTimeout(function(){
+					this_.find(".animate").removeAttr("style");
+				},1000)
+				init_page("#"+id);
+			}
+			
+		})
+		$(this).swipeDown(function(){
+			var num = parseInt($(this).attr("data-order"));
+			num--;
+			if(num == 0){
+				return;
+			}
+			$(".bottom_common").show();
+			var o = $(".main[data-order='"+num+"']");
+			if(o != undefined){
+				var id = o.attr("id");
+				o.css("animation","swipeDown 1s ease both");
+				var this_ = $(this);
+				setTimeout(function(){
+					this_.find(".animate").removeAttr("style");
+				},1000)
+				init_page("#"+id);
+			}
+		})
+	})
 }
